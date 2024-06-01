c9cbeecfc5a9abd8d7699e2793e6cb70869068dc
dibyaadhikaree <dibyaaadhikari@gmail.com>
2024-06-01 16:29:45 +0545

    middleware

--- server/app.js
+++ server/app.js
@@ -41,6 +41,7 @@ app.use((req, res, next) => {
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  //afd
   next();
 });
 
