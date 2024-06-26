d47d8dac0794e3d206e43aefcd87638d603f7ac6
dibyaadhikaree <dibyaaadhikari@gmail.com>
2024-06-26 19:15:43 +0545

    removed logs

--- client/src/features/bookings/BookingTable.jsx
+++ client/src/features/bookings/BookingTable.jsx
@@ -18,8 +18,6 @@ function BookingTable() {
   const bookings = data.data;
   const totalDocs = data.count;
 
-  console.log(bookings, "bookings  totalct : ", totalDocs !== 0);
-
   return (
     <Menus>
       <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
